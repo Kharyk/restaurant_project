64da84c67fa53c5527ad29daebd5e38db258ec4c
alinakharyk <alinakharyk@gmail.com>
2025-01-08 19:40:29 +0100

    allowed_hosts

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 from decouple import config
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='127.0.0.1')]
+ALLOWED_HOSTS = '.onrender.com'#[config('ALLOWED_HOSTS', default='127.0.0.1')]
 
 
 # Application definition
